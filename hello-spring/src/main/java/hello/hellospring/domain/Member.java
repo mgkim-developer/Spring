07d9353cbc1271af2d77ad30c825ff002278bd5a
mgkim-developer <mgkim.developer@gmail.com>
2022-05-22 21:48:32 +0900

    2022.05.22_(스프링 입문 - JPA)

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,13 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+//    @Column(name = "username")
     private String name;
 
     public long getId() {
