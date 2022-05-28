0a7a83247b8d1c7a30256d0e279ce3084ce31da5
mgkim-developer <mgkim.developer@gmail.com>
2022-05-28 21:44:32 +0900

    2022.05.28_(스프링 기본 - @Configuration과 싱글톤)

--- Spring-basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Spring-basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -22,4 +22,9 @@ public class OrderServiceImpl implements OrderService{
 
         return new Order(memberId, itemName, itmePrice, discountPrice);
     }
+
+    // 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
