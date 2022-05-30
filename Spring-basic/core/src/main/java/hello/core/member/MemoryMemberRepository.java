743ca8cfc460779c7aaa435d31256c8f067f69ad
mgkim-developer <mgkim.developer@gmail.com>
2022-05-30 18:32:09 +0900

    2022.05.30_(스프링 기본 - 컴포넌트 스캔과 의존관계 자동 주입 시작하기)

--- Spring-basic/core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ Spring-basic/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>(); // 실무에서는 동시성 문제때문에 컨커런트 해시맵 사용해야함
