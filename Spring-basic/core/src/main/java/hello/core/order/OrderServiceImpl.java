4434e1ebb5247764c6884df7d0ca24b9cb29a272
mgkim-developer <mgkim.developer@gmail.com>
2022-06-02 16:37:23 +0900

    2022.06.02_(스프링 기본 - 생성자 주입을 선택해라!)

--- Spring-basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Spring-basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,15 +9,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+    // 생성자 의존 관계
+    private  MemberRepository memberRepository;
+    private  DiscountPolicy discountPolicy;
 
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
     }
 
+    //  수정자 의존관계
+//    private MemberRepository memberRepository;
+//    private DiscountPolicy discountPolicy;
+//
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itmePrice) {
