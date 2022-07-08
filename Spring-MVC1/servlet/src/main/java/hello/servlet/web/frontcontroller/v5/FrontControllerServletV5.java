9a0316d7e1099868f76f45c318887b30150dcdd2
mgkim-developer <mgkim.developer@gmail.com>
2022-07-08 22:09:28 +0900

    2022.07.08_(스프링 MVC 1 - 유연한 컨트롤러2 - v5)

--- Spring-MVC1/servlet/src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ Spring-MVC1/servlet/src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -5,7 +5,11 @@ import hello.servlet.web.frontcontroller.MyView;
 import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import hello.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import hello.servlet.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -34,16 +38,23 @@ public class FrontControllerServletV5 extends HttpServlet {
         handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+
+        // V4추가
+        handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
     }
 
     private void initHandlerAdapters() {
         handlerAdapters.add(new ControllerV3HandlerAdapter());
+        handlerAdapters.add(new ControllerV4HandlerAdapter());
     }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         // MemberFormControllerV3 반환 ex)http://localhost:8080/front-controller/v5/v3/members/new-form 로 접속하면,
+        // MemberFormControllerV4 반환 ex)http://localhost:8080/front-controller/v5/v4/members/new-form 로 접곡하면,
         Object handler = getHandler(request);
 
         if (handler == null) {
@@ -52,6 +63,7 @@ public class FrontControllerServletV5 extends HttpServlet {
         }
 
         // ControllerV3HandlerAdapter
+        // ControllerV4HandlerAdapter
         MyHandlerAdapter adapter = getHandlerAdapter(handler);
 
         ModelView mv = adapter.handle(request, response, handler);
@@ -69,7 +81,7 @@ public class FrontControllerServletV5 extends HttpServlet {
 
     private MyHandlerAdapter getHandlerAdapter(Object handler) {
         for (MyHandlerAdapter adapter : handlerAdapters) {
-            if (adapter.supports(handler)) { // MemberFormControllerV3는 있어서 true 이므로
+            if (adapter.supports(handler)) { // MemberFormControllerV3는 있어서 true 이므로 // MemberFormControllerV4는 있어서 true 이므로
                 return adapter; // ControllerV3HandlerAdapter 를 반환
             }
         }
