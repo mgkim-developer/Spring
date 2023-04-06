ec86f4bdf3b300a26eab1c6e9f75a9e7d67b9b3a
mgkim-developer <mgkim.developer@gmail.com>
2023-04-06 10:23:01 +0900

    2023.04.06_(스프링 DB2 - 테스트 - 데이터 롤백)

--- Spring-DB2/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ Spring-DB2/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -5,9 +5,13 @@ import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import hello.itemservice.repository.memory.MemoryItemRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
@@ -19,12 +23,24 @@ class ItemRepositoryTest {
     @Autowired
     ItemRepository itemRepository;
 
+    @Autowired
+    PlatformTransactionManager transactionManager;
+    TransactionStatus status;
+
+    @BeforeEach
+    void beforeEach() {
+        // 트랜잭션 시작
+        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+    }
+
     @AfterEach
     void afterEach() {
         //MemoryItemRepository 의 경우 제한적으로 사용
         if (itemRepository instanceof MemoryItemRepository) {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
+        // 트랜잭션 롤백
+        transactionManager.rollback(status);
     }
 
     @Test
@@ -36,7 +52,7 @@ class ItemRepositoryTest {
         Item savedItem = itemRepository.save(item);
 
         //then
-        Item findItem = itemRepository.findById(item.getId()).get();
+        Item findItem = itemRepository.findById(item.getId()).get();    // 트랜잭션에서는 내가 만든 임시데이터는 커밋하지않아도 나는 조회가능
         assertThat(findItem).isEqualTo(savedItem);
     }
 
